7689f773a98803b61cc27d9bbfffa08c320d7ede
reyraa <ali@muzikie.com>
2024-10-08 22:06:22 +0200

    Define errors

--- programs/project_crowd_funding/src/errors.rs
+++ programs/project_crowd_funding/src/errors.rs
@@ -1,60 +1,3 @@
-// use anchor_lang::prelude::*;
-//
-// #[error_code]
-// pub enum CrowdfundingError {
-//     #[msg("The project has already reached its hard cap.")]
-//     HardCapReached,
-//
-//     #[msg("The contribution tier is invalid.")]
-//     InvalidContributionTier,
-//
-//     #[msg("The contribution amount is less than the minimum tier amount.")]
-//     InvalidContributionAmount,
-//
-//     #[msg("The project is not published.")]
-//     ProjectNotPublished,
-//
-//     #[msg("The project deadline has passed.")]
-//     DeadlinePassed,
-//
-//     #[msg("The contribution amount does not match the required tier amount.")]
-//     IncorrectAmount,
-//
-//     #[msg("Project is not in draft status.")]
-//     ProjectNotInDraft,
-//
-//     #[msg("No contribution tiers available.")]
-//     NoContributionTiers,
-//
-//     // Fixed error message to match the test
-//     #[msg("Maximum contribution tiers reached.")]
-//     MaxContributionTiersReached,  // Update this to match the test
-//
-//     #[msg("You are not authorized to add tiers.")]
-//     Unauthorized,
-//
-//     #[msg("The project cannot be finalized, as it is not in a finalizable state.")]
-//     ProjectNotFinalizable,  // Add this new variant
-//
-//     #[msg("Contributor not found for refund.")]
-//     ContributorNotFound,  // <--- Add this variant
-//
-//     #[msg("Deadline not passed.")]
-//     DeadlineNotPassed,
-//
-// 	#[msg("The project is not eligible for reimbursements.")]
-//     ProjectNotReimbursable,
-//
-//     #[msg("The contributor was not found or has no unreimbursed contributions.")]
-//     NoUnreimbursedContributions,
-//
-//     #[msg("The project does not have sufficient funds for reimbursement.")]
-//     InsufficientFunds,
-//
-//     #[msg("The contribution tier was not found.")]
-//     TierNotFound,
-// }
-
 use anchor_lang::prelude::*;
 
 #[error_code]
@@ -83,6 +26,9 @@ pub enum CrowdfundingError {
     #[msg("The project is not in Failing state.")]
     ProjectNotFailing,
 
+    #[msg("The project has failed.")]
+    ProjectFailed,
+
     #[msg("The project must have at least one contribution tier.")]
     NoContributionTiers,
 
@@ -95,6 +41,6 @@ pub enum CrowdfundingError {
     #[msg("The contribution amount does not match the required tier amount.")]
     IncorrectAmount,
 
-    #[msg("The escrow account does not have enough funds.")]
+    #[msg("The project account does not have enough funds.")]
     InsufficientFunds,
 }
