e41f2159b375a6458a318b67c771b8af47989f1d
curvesy <noobesang@gmail.com>
2024-12-16 19:32:23 +0330

    Update accounts to use TokenInterface for compatibility with the latest TokenAccount on Solana

--- programs/project_crowd_funding/src/instructions/contribute.rs
+++ programs/project_crowd_funding/src/instructions/contribute.rs
@@ -1,57 +1,73 @@
 use anchor_lang::prelude::*;
 use anchor_spl::associated_token::AssociatedToken;
-use anchor_spl::token_interface::{Mint, TokenAccount};
-use anchor_spl::token::{Token, Transfer as SplTransfer};
+use anchor_spl::token_interface::{Mint, TokenAccount, TransferChecked, transfer_checked};
+use anchor_spl::token::Token;
 
-// use crate::errors::CrowdfundingError;
-use crate::state::Project;
+use crate::errors::CrowdfundingError;
+use crate::state::{Project, ProjectStatus};
 
 pub fn contribute_spl_tokens(ctx: Context<Contribute>, amount: u64, tier_id: u64) -> Result<()> {
     let source = &ctx.accounts.contributer_ata;
     let destination = &ctx.accounts.project_ata;
     let token_program = &ctx.accounts.token_program;
     let authority = &ctx.accounts.contributer;
-    // let project: &mut Account<'_, Project> = &mut ctx.accounts.project;
-// 
-//     msg!("Contributer: {:?}", ctx.accounts.contributer.key);
-// 
-//     // Check if the tier ID exists in the project's tiers
-//     require!(
-//         project.contribution_tiers.iter().any(|tier| tier.tier_id == tier_id),
-//         CrowdfundingError::TierNotFound
-//     );
+    let mint = &ctx.accounts.usdc_mint;
+    let project: &mut Account<'_, Project> = &mut ctx.accounts.project;
+
+    msg!("Contributer: {:?}", ctx.accounts.contributer.key);
+
+    // Check if the tier ID exists in the project's tiers
+    require!(
+        project.contribution_tiers.iter().any(|tier| tier.tier_id == tier_id),
+        CrowdfundingError::TierNotFound
+    );
 
     // Validate the project's state
-    // require!(project.status != ProjectStatus::Draft, CrowdfundingError::ProjectNotPublished);
-    // require!(project.status != ProjectStatus::SoldOut, CrowdfundingError::HardCapReached);
-    // require!(
-    //     !(project.status == ProjectStatus::Failed || project.status == ProjectStatus::Failing),
-    //     CrowdfundingError::ProjectFailed
-    // );
+    require!(project.status != ProjectStatus::Draft, CrowdfundingError::ProjectNotPublished);
+    require!(project.status != ProjectStatus::SoldOut, CrowdfundingError::HardCapReached);
+    require!(
+        !(project.status == ProjectStatus::Failed || project.status == ProjectStatus::Failing),
+        CrowdfundingError::ProjectFailed
+    );
 
-    // // Ensure the contributor's balance is sufficient
-    // require!(
-    //     source.amount >= amount,
-    //     CrowdfundingError::InsufficientFunds
-    // );
+    // Ensure the contributor's balance is sufficient
+    require!(
+        source.amount >= amount,
+        CrowdfundingError::InsufficientFunds
+    );
 
     // Ensure the project has not reached its deadline
-    // let current_timestamp: i64 = Clock::get()?.unix_timestamp;
-    // require!(
-    //     current_timestamp <= project.deadline,
-    //     CrowdfundingError::DeadlineNotPassed
-    // );
+    let current_timestamp: i64 = Clock::get()?.unix_timestamp;
+    require!(
+        current_timestamp <= project.deadline,
+        CrowdfundingError::DeadlineNotPassed
+    );
+    
+    let transfer_accounts_options = TransferChecked {
+        from: source.to_account_info(),
+        mint: mint.to_account_info(),
+        to: destination.to_account_info(),
+        authority: authority.to_account_info(), // Correct signer
+    };
+    
+    let cpi_context = CpiContext::new(token_program.to_account_info(), transfer_accounts_options);
+    
+    transfer_checked(cpi_context, amount, mint.decimals)?;
+    
+    
+    project.current_funding += amount;
+    
 
     // TODO: Handle tier-specific contributions here
 
     // Transfer tokens from contributor to the project
-    let cpi_accounts = SplTransfer {
-        from: source.to_account_info(),
-        to: destination.to_account_info(),
-        authority: authority.to_account_info(),
-    };
-    let cpi_program = token_program.to_account_info();
-    anchor_spl::token::transfer(CpiContext::new(cpi_program, cpi_accounts), amount)?;
+    // let cpi_accounts = SplTransfer {
+    //     from: source.to_account_info(),
+    //     to: destination.to_account_info(),
+    //     authority: authority.to_account_info(),
+    // };
+    // let cpi_program = token_program.to_account_info();
+    // anchor_spl::token::transfer(CpiContext::new(cpi_program, cpi_accounts), amount)?;
 
     msg!("Successfully contributed {} USDC from fan to the project.", amount);
     Ok(())
@@ -69,27 +85,22 @@ pub struct Contribute<'info> {
     #[account(
         mut,
         associated_token::mint = usdc_mint,
-        associated_token::authority = contributer,
-        constraint = contributer_ata.owner == contributer.key(),
-        constraint = contributer_ata.mint == usdc_mint.key()
+        associated_token::authority = contributer
     )]
     pub contributer_ata: InterfaceAccount<'info, TokenAccount>, // Contributor's associated token account
 
     #[account(
-        init,
+        init_if_needed,
         payer = contributer,
         associated_token::mint = usdc_mint,
-        associated_token::authority = project,
-        constraint = project_ata.owner == token_program.key(),
-        constraint = project_ata.mint == usdc_mint.key()
+        associated_token::authority = project
     )]
     pub project_ata: InterfaceAccount<'info, TokenAccount>, // Project's associated token account
 
     pub usdc_mint: InterfaceAccount<'info, Mint>, // USDC Mint
     
-    pub associated_token_program: Program<'info, AssociatedToken>, 
+    pub associated_token_program: Program<'info, AssociatedToken>,
     pub token_program: Program<'info, Token>,
     pub system_program: Program<'info, System>,
-
 }
 
